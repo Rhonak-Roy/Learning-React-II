dac9bf3f5ff8f1efb7d1e903d49389cefa563e44
Rhonak Roy <rhonakroy2004@gmail.com>
2025-08-22 01:13:24 +0530

    adjustments

--- Header.jsx
+++ Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header>
             <img src={chefClaudeLogo} alt="chef-img"/>
-            <h1>Chef Claude</h1>
+            <h1>Chef Roy</h1>
         </header>
     )
 }
